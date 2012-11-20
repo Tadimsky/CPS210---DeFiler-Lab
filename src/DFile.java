d5f6db94b3f4af9224777a245ba2420177b6144e
Patrick Royal <arealpanickytroll@gmail.com>
2012-11-20 18:47:14 -0500

    Created INode

--- src/DFile.java
+++ src/DFile.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class DFile {
     
-    private List<byte[]> _blocks;
+    private List<INode> _blocks;
     private DFileID _dFID;
     
     public DFile(DFileID dFID, byte[] ubuffer) {
