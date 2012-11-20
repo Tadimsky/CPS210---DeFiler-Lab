3ca3859f2277a483c3450f6e5ef918bb0ffef199
Patrick Royal <arealpanickytroll@gmail.com>
2012-11-20 18:54:28 -0500

    Created method stubs for INode

--- src/INode.java
+++ src/INode.java
@@ -1,4 +1,12 @@
 
 public class INode {
+    public INode next;
+    public INode prev;
+    public byte[] data;
 
+    public INode(INode p, INode n, byte[] d) {
+        next = n;
+        prev = p;
+        data = d;
+    }
 }
