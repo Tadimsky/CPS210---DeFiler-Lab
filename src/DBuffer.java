ead31521b2df264d61fafc072e40f41dc8a6f546
Patrick Royal <arealpanickytroll@gmail.com>
2012-11-20 18:31:09 -0500

    Implemented method stubs for DBuffer

--- src/DBuffer.java
+++ src/DBuffer.java
@@ -1,4 +1,46 @@
 
 public class DBuffer {
 
+    public void startFetch() {
+        // TODO
+    }
+    
+    public void startPush() {
+        // TODO
+    }
+    
+    public boolean checkValid() {
+        //TODO
+        return false;
+    }
+    
+    public boolean waitValid() {
+        // TODO
+        return false;
+    }
+    
+    public boolean checkClean() {
+        // TODO
+        return false;
+    }
+    
+    public boolean waitClean() {
+        // TODO
+        return false;
+    }
+    
+    public boolean isBusy() {
+        // TODO
+        return false;
+    }
+    
+    public int read(byte[] ubuffer, int startOffset, int count) {
+        // TODO
+        return 0;
+    }
+    
+    public int write(byte[] ubuffer, int startOffset, int count) {
+        // TODO
+        return 0;
+    }
 }
