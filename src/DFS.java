e80ff46732c25262aef90b5e7df1ade248c7400c
Patrick Royal <arealpanickytroll@gmail.com>
2012-11-20 18:20:49 -0500

    Implemented method stubs for DFS

--- src/DFS.java
+++ src/DFS.java
@@ -1,4 +1,22 @@
+import java.util.List;
+
 
 public class DFS {
 
+    public void createFile(int ID, int size) {
+        // TODO
+    }
+    
+    public void destroyFile(int ID) {
+        // TODO
+    }
+    
+    public List<Byte> readFile(int ID, int bytes) {
+        // TODO
+        return null;
+    }
+    
+    public void writeFile(int ID, List<Byte> contents) {
+        // TODO
+    }
 }
