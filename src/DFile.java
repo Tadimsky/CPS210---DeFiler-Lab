63fc908a102a7a4b0827a0100347fa9bc2551a6b
Patrick Royal <arealpanickytroll@gmail.com>
2012-11-20 18:45:24 -0500

    Created constructor for DFile

--- src/DFile.java
+++ src/DFile.java
@@ -1,4 +1,13 @@
+import java.util.List;
+
 
 public class DFile {
+    
+    private List<byte[]> _blocks;
+    private DFileID _dFID;
+    
+    public DFile(DFileID dFID, byte[] ubuffer) {
+        // TODO
+    }
 
 }
