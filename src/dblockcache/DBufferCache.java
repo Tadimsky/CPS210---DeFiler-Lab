a8e333016d16eb999a3309f3c5ebd02c40a6cc26
Patrick Royal <arealpanickytroll@gmail.com>
2012-11-20 19:35:55 -0500

    Added documentation for DBufferCache

--- src/dblockcache/DBufferCache.java
+++ src/dblockcache/DBufferCache.java
@@ -7,19 +7,35 @@ public class DBufferCache {
 		_cacheSize = cachesize;
 	}
 
-    public DBuffer getBlock(int BlockID) {
+	/**
+	 * Get buffer for block specified by blockID
+	 * The buffer is "held" until the caller releases it
+	 * A "held" buffer cannot be evicted; its block ID cannot change.
+	 * @param BlockID name of block
+	 */
+    public DBuffer getBlock(int blockID) {
         // TODO
         return null;
     }
     
+    /**
+     * Release the buffer so that it may be eligible for eviction.
+     * @param dbuf buffer to be released
+     */
     public void releaseBlock(DBuffer dbuf) {
         // TODO
     }
     
+    /**
+     * Write back all dirty blocks tot he volume, and wait for completion.
+     */
     public void sync() {
         // TODO
     }
     
+    /**
+     * Called by releaseBlock to determine heuristically which block to release.
+     */
     public DBuffer getLRUBlock() {
         // TODO
         return null;
