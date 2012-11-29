f6b29edff36446ac1a272eb0549e91974119bb9e
Patrick Royal <arealpanickytroll@gmail.com>
2012-11-28 22:11:47 -0500

    Worked out and implemented the LRU algorithm for buffer access

--- src/dblockcache/DBufferCache.java
+++ src/dblockcache/DBufferCache.java
@@ -2,45 +2,47 @@ package dblockcache;
 
 import java.util.TreeSet;
 
-public class DBufferCache {
-	private int _cacheSize;
-	private TreeSet<DBuffer> _bufferList;
-	
-	public DBufferCache(int cachesize) {
-		_cacheSize = cachesize;
-	}
 
-	/**
-	 * Get buffer for block specified by blockID
-	 * The buffer is "held" until the caller releases it
-	 * A "held" buffer cannot be evicted; its block ID cannot change.
-	 * @param BlockID name of block
-	 */
-    public DBuffer getBlock(int blockID) {
+public class DBufferCache {
+    private int _cacheSize;
+    private SortedDBuffer _bufferList;
+
+    public DBufferCache (int cachesize) {
+        _cacheSize = cachesize;
+    }
+
+    /**
+     * Get buffer for block specified by blockID
+     * The buffer is "held" until the caller releases it
+     * A "held" buffer cannot be evicted; its block ID cannot change.
+     * 
+     * @param BlockID name of block
+     */
+    public DBuffer getBlock (int blockID) {
         // TODO
         return null;
     }
-    
+
     /**
      * Release the buffer so that it may be eligible for eviction.
+     * 
      * @param dbuf buffer to be released
      */
-    public void releaseBlock(DBuffer dbuf) {
+    public void releaseBlock (DBuffer dbuf) {
         // TODO
     }
-    
+
     /**
      * Write back all dirty blocks tot he volume, and wait for completion.
      */
-    public void sync() {
+    public void sync () {
         // TODO
     }
-    
+
     /**
      * Called by releaseBlock to determine heuristically which block to release.
      */
-    public DBuffer getLRUBlock() {
-        // TODO
-        return null;
+    public DBuffer getLRUBlock () {
+        return _bufferList.getLRUNode().getBuffer();
     }
 }
---
+++ src/dblockcache/SortedDBuffer.java
@@ -0,0 +1,88 @@
+package dblockcache;
+
+/**
+ * This class implements a binary search tree for DBuffers that handles the
+ * heuristic algorithm for finding the least-recently-used buffer.
+ * 
+ */
+public class SortedDBuffer {
+
+    private DBuffer _buffer;
+    private SortedDBuffer _left;
+    private SortedDBuffer _right;
+    private boolean _direction;
+
+    /**
+     * Constructor for sorteddbuffer
+     * 
+     * @param buffer the actual data
+     * @param direction a bit indicating whether the LRU item is to the left or
+     *        the right
+     */
+    public SortedDBuffer (DBuffer buffer, boolean direction) {
+        _buffer = buffer;
+        _direction = direction;
+    }
+
+    /**
+     * Adds a node to the proper location in the binary search tree
+     * 
+     * @param buffer data for the new node
+     */
+    public void addNode (DBuffer buffer) {
+        SortedDBuffer currentNode = this;
+        if (buffer.getBlockID() < currentNode.getBuffer().getBlockID()) {
+            if (currentNode.getLeft() == null) {
+                currentNode.setLeft(buffer);
+                return;
+            }
+            currentNode.getLeft().addNode(buffer);
+        }
+        else if (buffer.getBlockID() > currentNode.getBuffer().getBlockID()) {
+            if (currentNode.getRight() == null) {
+                currentNode.setRight(buffer);
+                return;
+            }
+            currentNode.getRight().addNode(buffer);
+        }
+    }
+
+    /**
+     * Finds and returns a good approximation of the least-recently-used buffer.
+     * As it traverses the tree, it flips the booleans associated with each node
+     * it passes so that the next access will yield a different buffer.
+     */
+    public SortedDBuffer getLRUNode () {
+        if (!_direction && _left == null) return this;
+        if (_direction && _right == null) return this;
+        SortedDBuffer child;
+        if (!_direction)
+            child = _left.getLRUNode();
+        else child = _right.getLRUNode();
+        _direction = !_direction;
+        return child;
+    }
+
+    /**
+     * Returns the data associated with a given SortedDBuffer.
+     */
+    public DBuffer getBuffer () {
+        return _buffer;
+    }
+
+    private SortedDBuffer getLeft () {
+        return _left;
+    }
+
+    private SortedDBuffer getRight () {
+        return _right;
+    }
+
+    private void setLeft (DBuffer buffer) {
+        _left = new SortedDBuffer(buffer, true);
+    }
+
+    private void setRight (DBuffer buffer) {
+        _right = new SortedDBuffer(buffer, false);
+    }
+}
